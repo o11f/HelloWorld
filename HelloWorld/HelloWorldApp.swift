200136ba55754e66a45754dd53a0ca957e662872
Nikita Anibroev <Nikki.o11f@gmail.com>
2021-07-23 18:04:44 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Nikita Anibroev on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
